f80ccf4bb3449b8a16dd567fe408f565b1c5ee03
tamvu07 <vuminhtam.sdb@gmail.com>
2021-05-13 21:31:09 +0700

    !Add file .gitignore

---
+++ Jump1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Jump1
+//
+//  Created by vuminhtam on 5/13/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
